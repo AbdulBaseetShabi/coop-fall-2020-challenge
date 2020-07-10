99ef9a1d3fe138c05b4423054b35a2544430fe6e
Abdul-Baseet Shabi <44884500+AbdulBaseetShabi@users.noreply.github.com>
2020-07-10 14:59:34 -0400

    final

--- solution_javascript.js
+++ solution_javascript.js
@@ -1,18 +1,54 @@
 class EventSourcer {
   constructor() {
     this.value = 0;
+    this.changes = {};
+    this.length = 0;
+    this.current = 0;
   }
 
   add(num) {
     this.value += num;
+    this.changes[Number.toString(this.length)] = {
+      'event' : 'add',
+      'value': num,
+      'change_id' : this.length
+    };
+    this.current = this.length;
+    this.length += 1;
   }
+
   subtract(num) {
     this.value -= num;
+    this.changes[Number.toString(this.length)] = {
+      'event' : 'sub',
+      'value': num,
+      'solution' : this.value,
+      'change_id' : this.length
+    };
+    this.current = this.length;
+    this.length += 1;
+
+    console.log(this.changes);
   }
-  undo() {}
-  redo() {}
-  bulk_undo(num) {}
-  bulk_redo(num) {}
+
+  undo() {
+    let change = this.changes[Number.toString(this.current - 1)]
+    this.value = this.changes['solution']
+    this.current = this.changes['change_id']
+  }
+  redo() {
+    let change = this.changes[Number.toString(this.current + 1)]
+    this.value = this.changes['solution']
+    this.current = this.changes['change_id']
+  }
+  bulk_undo(num) {
+    let change = this.changes[Number.toString((this.current + num))]
+    this.value = this.changes['solution']
+  }
+  bulk_redo(num) {
+    let change = this.changes[Number.toString(this.length - num)]
+    this.value = this.changes['solution']
+  }
 }
 
 // ----- Do not modify anything below this line (needed for test suite) ------
